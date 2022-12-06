12fb7f570bb397002dfda32019842a2d45203d8d
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2022-12-06 13:34:18 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -173,10 +173,9 @@ def quotefunc(img, msg):
 async def quote(ctx: commands.Context, user: discord.Member):
     channel = ctx.channel
     msg = ""
-    prefix = await bot.get_prefix()
     async for i in channel.history(limit=50):
         if i.author == user:
-            if not i.content.lower().startswith(prefix) and not "quote" in i.content.lower():
+            if not i.content.lower().startswith("h!") and not "quote" in i.content.lower():
                 msg = i.id
                 break
     img = user.display_avatar
